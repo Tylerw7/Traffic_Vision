747b08839048e0842f46646f9da38770b945fc94
Tylerw7 <t4wettach@gmail.com>
2026-08-08 15:47:58 -0700

    video render

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#
+import cv2
+
+cap = cv2.VideoCapture("sf_traffic.mp4")
+
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+delay = int(700 / fps)
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        break
+
+    cv2.imshow("Traffic", frame)
+    if cv2.waitKey(delay) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()    
